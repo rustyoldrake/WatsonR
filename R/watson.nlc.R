64b24bc15337cd053c2e6c0c02eb55d25b693dfa
joe.dumoulin <joe.dumoulin@gmail.com>
2017-05-10 11:46:57 -0700

    mapUnicode in getUrl

--- R/watson.nlc.R
+++ R/watson.nlc.R
@@ -72,7 +72,7 @@ watson.nlc.processtext <- function(creds, classifier_id, query_text){
   base_url_nlc = "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/"
   query_text <- utils::URLencode(query_text)
   data <- RCurl::getURL(paste(base_url_nlc,classifier_id,"/classify","?text=", query_text,sep=""),
-                        userpwd = username_password, .encoding = 'UTF-8')
+                        userpwd = username_password, .encoding = 'UTF-8', .mapUnicode = TRUE)
   return(rjson::fromJSON(RCurl::htmlParse(data, encoding = 'UTF-8')))
 }
 ### end of function
